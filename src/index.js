96fd150efe1c632e1f9b6b99f97835f5067d69b9
Shad Azam <58129443+shadazam08@users.noreply.github.com>
2023-05-20 14:14:17 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
+const dotenv = require("dotenv");
+dotenv.config()
 
 const { API_PORT } = process.env;
 const port = process.env.PORT || API_PORT;
