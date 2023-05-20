814c1b0b4ac8f82516fd16b0d324d602df9467de
Shad Azam <58129443+shadazam08@users.noreply.github.com>
2023-05-20 14:13:05 +0530

    Update user.js

--- src/model/user.js
+++ src/model/user.js
@@ -1,7 +1,21 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  //your code goes here
+  first_name: {
+    type:String,
+    required:true
+  },
+  last_name: {
+    type:String,
+    required:true
+  },
+  email: {
+    type:String,
+    required:true,
+  },
+  password: {
+    type:String,
+    required:true
+  }
 });
-
 module.exports = mongoose.model("user", userSchema);
